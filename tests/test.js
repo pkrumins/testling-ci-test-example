d27318b2b5db5f89018ac3c79685829ec061c3bd
Peteris Krumins <peteris.krumins@gmail.com>
2013-01-19 01:19:06 +0200

    use deepEqual not equal

--- tests/test.js
+++ tests/test.js
@@ -3,10 +3,10 @@ var exampleLib = require('../index.js');
 
 test('timesTwo test', function (t) {
   t.plan(1);
-  t.equal(exampleLib.timesTwo([1,2,3]), [2,4,6]);
+  t.deepEqual(exampleLib.timesTwo([1,2,3]), [2,4,6]);
 });
 
 test('timesThree test', function (t) {
   t.plan(1);
-  t.equal(exampleLib.timesThree([1,2,3]), [3,6,9]);
+  t.deepEqual(exampleLib.timesThree([1,2,3]), [3,6,9]);
 });
